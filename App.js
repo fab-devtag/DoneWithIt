03adb96be43996677201408785f81904a00ef5fc
Fabien <fabien.sens@free.fr>
2020-07-08 15:18:40 +0200

    Building login form

--- App.js
+++ App.js
@@ -14,40 +14,14 @@ import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
-import Screen from './app/components/Screen';
+import LoginScreen from './app/screens/LoginScreen';
 
+import Screen from './app/components/Screen';
 import AppButton from './app/components/AppButton';
 import AppCard from './app/components/AppCard';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
-const categories = [
-  {
-    label: 'Furniture',
-    value: 1,
-  },
-  {
-    label: 'Clothing',
-    value: 2,
-  },
-  {
-    label: 'Cameras',
-    value: 3,
-  },
-];
-
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
